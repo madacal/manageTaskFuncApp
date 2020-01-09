a0425c1cc038ec33d61aa44b7454d141721937b6
madacal <madacal@gmail.com>
2020-01-09 21:56:19 +0000

    get updated

--- TasksGet/index.js
+++ TasksGet/index.js
@@ -1,12 +1,23 @@
-module.exports = function (context, req) {
+module.exports = async function (context, req) {
+
+    var tasks = [] ;
+
     for(var i =0; i< context.bindings.productDocument.length; i++) {
         let product = context.bindings.productDocument[i];
         context.log('id', product.id); 
         context.log('name', product.name);
+        var task = {};
+            task.id = product.id;
+            task.name = product.name;
+            task.owner = product.owner;
+            task.list = product.list;
+            task.status = product.status;
+        
+        tasks.push(task);
     }
-  
+
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: "Ran ProductsGet"
+        body:tasks
     };
   };
